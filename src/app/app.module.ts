d9d09665b15fd0e2b0b61d016c27590e242589e6
Will <doggy.huang@gmail.com>
2018-09-17 10:28:55 +0800

    練習各種不同的路由定義規則

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Page1Component,
-    Page2Component
+    Page2Component,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
